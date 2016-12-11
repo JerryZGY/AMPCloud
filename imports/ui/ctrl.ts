96bf83a57b3e2c5ffd3fe26027d712b1a0ab77b3
Jerry Zang <jerry@ltc.tw>
2016-12-12 01:41:09 +0800

    Add ctrl template

--- imports/ui/ctrl.ts
+++ imports/ui/ctrl.ts
@@ -1,1 +1,10 @@
 import './ctrl.html';
+import { Projects, IProject } from '../../lib/collections';
+
+Template['ctrl'].onCreated(() => {
+    Meteor.subscribe('projects');
+});
+
+Template['ctrl'].helpers({
+    projects: () => Projects.find(),
+});
