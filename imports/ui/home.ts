c1ac481500715236d3a711106832ae24f3106ead
Jerry Zang <jerry@ltc.tw>
2016-12-12 01:40:51 +0800

    Add project template

--- imports/ui/home.ts
+++ imports/ui/home.ts
@@ -1,1 +1,11 @@
 import './home.html';
+import './project';
+import { Projects, IProject } from '../../lib/collections';
+
+Template['home'].onCreated(() => {
+    Meteor.subscribe('projects');
+});
+
+Template['home'].helpers({
+    projects: () => Projects.find(),
+});
