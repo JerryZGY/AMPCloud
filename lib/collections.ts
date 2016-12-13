480aa0dcb487679361a5d18c279be4349d262bef
Jerry Zang <jerry@ltc.tw>
2016-12-13 21:33:54 +0800

    Modify collections

--- lib/collections.ts
+++ lib/collections.ts
@@ -3,21 +3,30 @@ import { Mongo } from 'meteor/mongo';
 export const Projects = new Mongo.Collection<IProject>('projects');
 
 interface ICollection {
-  _id?: string;
+    _id?: string;
 }
 
 export interface IProject extends ICollection {
-    projectNo: number;
-    projectName: string;
-    buildDate: Date;
-    design?: Design;
+    projectNo?: number;
+    projectName?: string;
+    buildDate?: Date;
+    design?: Design; // array
     machining?: Machining;
     molding?: Molding;
 }
 
 type Design = {
-    designNo: number;
-    designProgress: number;
+    orderNum?: string;
+    moldNum?: string;
+    Template?: string;
+    cumstomerId?: string;
+    productName?: string;
+    orderDate?: string;
+    deadlineTime?: string;
+    Event?: string;
+    Data?: string;
+    Date?: string;
+    Percent?: number;
 };
 
 type Machining = {
