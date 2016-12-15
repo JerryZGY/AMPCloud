22ee6de71a293843e93a04235c65ad9b9cc3ac6d
Jerry Zang <jerry@ltc.tw>
2016-12-15 18:32:09 +0800

    Add collections comment

--- lib/collections.ts
+++ lib/collections.ts
@@ -16,13 +16,13 @@ export interface IProject extends ICollection {
 }
 
 type Design = {
-    orderNum?: string;
-    moldNum?: string;
-    Template?: string;
-    cumstomerId?: string;
-    productName?: string;
-    orderDate?: string;
-    deadlineTime?: string;
+    orderNum?: string; // 訂單編號
+    moldNum?: string; // 模具編號
+    Template?: string; // 模具設計樣板
+    cumstomerId?: string; // 客戶編號
+    productName?: string; // 品名
+    orderDate?: string; // 訂單時間
+    deadlineTime?: string; // 完工時間
     Event?: string;
     Data?: string;
     Date?: string;
