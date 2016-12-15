5b1cfec71afb2f9b2fc324b305c4b9bf4af3e033
Jerry Zang <jerry@ltc.tw>
2016-12-15 18:32:49 +0800

    Implement home page

--- imports/ui/home.ts
+++ imports/ui/home.ts
@@ -1,11 +1,30 @@
 import './home.html';
 import './project';
+import { Mongo } from 'meteor/mongo';
 import { Projects, IProject } from '../../lib/collections';
 
+
 Template['home'].onCreated(() => {
-    Meteor.subscribe('projects');
+    Meteor.subscribe('projects', () => {
+        const projects = Projects.find();
+        projects.observe({
+            changed(newDoc: any) {
+                const _id = newDoc._id;
+                $(`#${_id} .design`).data('progress').set(newDoc.design.Percent);
+            },
+        });
+    });
 });
 
 Template['home'].helpers({
     projects: () => Projects.find(),
+});
+
+Template['home'].events({
+    'click #create'() {
+        console.log(Meteor.call('projects.insert'));
+    },
+    'click #design'() {
+        Meteor.call('design.update');
+    },
 });
