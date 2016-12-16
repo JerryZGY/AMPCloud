897b2c774eb80853b3be27521d3cc33746d8a72b
Jerry Zang <jerry@ltc.tw>
2016-12-16 09:58:33 +0800

    Format main page code

--- client/main.ts
+++ client/main.ts
@@ -14,7 +14,7 @@ import ClientRouter from '../lib/clientRouter';
 
 const router = new ClientRouter();
 router.initRoutes([
-  { name: 'home', path: '/' },
-  { name: 'ctrl', path: '/ctrl' },
-  { name: 'design', path: '/design/:id' },
+    { name: 'home', path: '/' },
+    { name: 'ctrl', path: '/ctrl' },
+    { name: 'design', path: '/design/:id' },
 ]);
