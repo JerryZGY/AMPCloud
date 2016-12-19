6d9f257534ae7b8e033b53ac00e7039df9f06b63
Jerry Zang <jerry@ltc.tw>
2016-12-19 18:16:08 +0800

    Add demo home page events

--- imports/ui/home.ts
+++ imports/ui/home.ts
@@ -4,14 +4,13 @@ import './project';
 import { Mongo } from 'meteor/mongo';
 import { Projects } from '../../lib/collections';
 
-
 Template['home'].onCreated(() => {
     Meteor.subscribe('projects', () => {
         const projects = Projects.find();
         projects.observe({
             changed(newDoc: any) {
                 const _id = newDoc._id;
-                $(`#${_id} .design`).data('progress').set(newDoc.design.Percent);
+                $(`#${_id} .design`).data('progress').set(0);
             },
         });
     });
@@ -24,9 +23,9 @@ Template['home'].helpers({
 
 Template['home'].events({
     'click #create'() {
-        console.log(Meteor.call('projects.insert'));
+        console.log(Meteor.call('createProject', { projectNo: 'PJ20161206001' }));
     },
     'click #design'() {
-        Meteor.call('design.update');
+        Meteor.call('updateDesign', { projectNo: 456 });
     },
 });
