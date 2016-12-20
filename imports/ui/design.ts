5ae26ae6ef2b48fae5ab5c2f9e506bcc2281da55
Jerry Zang <jerry@ltc.tw>
2016-12-20 12:19:30 +0800

    Migrate ctrl page to design page

--- imports/ui/design.ts
+++ imports/ui/design.ts
@@ -3,4 +3,10 @@ import './design.scss';
 
 Template['design'].onCreated(() => {
     $('body').attr('class', 'design');
+    $('#icon').attr('class', 'mif-pencil');
+    $('#identify').text('PJ20161206001');
+});
+
+Template['design'].onDestroyed(() => {
+    $('#identify').text('');
 });
