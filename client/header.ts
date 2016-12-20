cce8c127694d74449be1f803aeee25ae1357d319
Jerry Zang <jerry@ltc.tw>
2016-12-20 17:07:33 +0800

    Implement dynamic header

--- client/header.ts
+++ client/header.ts
@@ -1,2 +1,29 @@
 import './header.html';
 import './header.scss';
+import { Router } from './main';
+
+let id;
+Template['header'].onCreated(() => {
+    Router.getContext((current) => {
+        id = current.params.id;
+    });
+});
+
+Template['header'].helpers({
+    id: () => Router.get('id'),
+});
+
+Template['header'].events({
+    'click .app-bar>.design'() {
+        Router.go(`/design/${id}`);
+    },
+    'click .app-bar>.scheduling'() {
+        Router.go(`/scheduling/${id}`);
+    },
+    'click .app-bar>.machining'() {
+        Router.go(`/machining/${id}`);
+    },
+    'click .app-bar>.molding'() {
+        Router.go(`/molding/${id}`);
+    },
+});
