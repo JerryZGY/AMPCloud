3b19c207b642b59b613fd4b9986519d787e8d122
Jerry Zang <jerry@ltc.tw>
2016-12-20 17:05:26 +0800

    Implement project template event binding

--- imports/ui/project.ts
+++ imports/ui/project.ts
@@ -1,2 +1,21 @@
 import './project.html';
 import './project.scss';
+import { Router } from '../../client/main';
+
+Template['project'].events({
+    'click .ctrl'(e, tmpl) {
+        Router.go(`/ctrl/${tmpl.data._id}`);
+    },
+    'click .design'(e, tmpl) {
+        Router.go(`/design/${tmpl.data._id}`);
+    },
+    'click .scheduling'(e, tmpl) {
+        Router.go(`/scheduling/${tmpl.data._id}`);
+    },
+    'click .machining'(e, tmpl) {
+        Router.go(`/machining/${tmpl.data._id}`);
+    },
+    'click .molding'(e, tmpl) {
+        Router.go(`/molding/${tmpl.data._id}`);
+    },
+});
