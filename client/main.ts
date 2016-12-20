44a79aab7f6e19784c32acbaadd35284fc66f86e
Jerry Zang <jerry@ltc.tw>
2016-12-20 12:19:04 +0800

    Add header page template to main page

--- client/main.ts
+++ client/main.ts
@@ -6,15 +6,15 @@ import '../node_modules/metro-dist/css/metro-rtl.min.css';
 import '../node_modules/metro-dist/css/metro-schemes.min.css';
 import '../node_modules/metro-dist/js/metro.min.js';
 
+import './header';
 import '../imports/ui/home';
 import '../imports/ui/ctrl';
 import '../imports/ui/design';
-
 import ClientRouter from '../lib/clientRouter';
 
 const router = new ClientRouter();
 router.initRoutes([
     { name: 'home', path: '/' },
-    { name: 'ctrl', path: '/ctrl' },
-    { name: 'design', path: '/design/:id' },
+    { name: 'ctrl', path: '/ctrl/:id' },
+    { name: 'design', path: '/design' },
 ]);
