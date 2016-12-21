74c9feff3b1b8a519f1977ba9feae52840c44993
Jerry Zang <jerry@ltc.tw>
2016-12-21 11:43:47 +0800

    Implement createProject, notifyDesign, updateDesign meteor method

--- server/methods.ts
+++ server/methods.ts
@@ -4,12 +4,36 @@ import * as Models from '../lib/models';
 
 Meteor.methods({
     'createProject'(data: Models.Project) {
-        Projects.upsert({ _id: data.projectNo }, data);
+        const _id = data.projectNo;
+        data.logs = [data.projectEvent];
+        delete data.projectEvent;
+        data.designHistory = [data.design];
+        data.createdAt = new Date();
+        Projects.upsert({ _id }, data);
+        Meteor.call('notifyDesign', _id);
     },
-    'updateDesign'(design: Models.Design) {
-        const _id = design.projectNo;
-        delete design.projectNo;
-        Projects.update({ _id }, { $push: { designs: design } });
+    async 'notifyDesign'(_id: string) {
+        const project = Projects.findOne({ _id });
+        Projects.update({ _id }, { $push: { logs: 'Notify design' } });
+        // const opt = {
+        //     body: JSON.stringify({ data: project.design }),
+        //     json: true,
+        //     headers: { 'Content-Type': 'application/json' },
+        // };
+        // const designAPI = 'http://140.135.96.39/icmold/AMPWebService2.asmx/InsertProjInfo';
+        // const response = await got.post(designAPI, opt);
+        // if (response) {
+        //     Projects.update({ _id }, { $push: { logs: 'Notify design' } });
+        // }
+    },
+    'updateDesign'(data: Models.Design) {
+        const _id = data.projectNo;
+        const project = Projects.findOne({ _id });
+        project.logs.push(data.projectEvent);
+        project.designHistory.push(data);
+        data.updatedAt = new Date();
+        project.design = data;
+        Projects.update({ _id }, project);
     },
     'updateMachining'(machining: Models.Machining) {
         const _id = machining.projectNo;
