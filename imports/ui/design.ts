229238a3e8fbf5e03049ddb4e8690f21637c9baa
Jerry Zang <jerry@ltc.tw>
2016-12-22 10:45:20 +0800

    Modify design page render method

--- imports/ui/design.ts
+++ imports/ui/design.ts
@@ -4,40 +4,42 @@ import * as d3 from 'd3';
 import { Projects } from '../../lib/collections';
 import { Router } from '../../client/main';
 
+const radius = 45;
+let progressRing = null;
+let autorunHandle: Tracker.Computation = null;
+let subscribeHandle: Meteor.SubscriptionHandle = null;
+
 Template['design'].onCreated(function () {
     $('body').attr('class', 'design');
-    this.subscribe('projects', () => {
-        this.autorun(() => {
+    subscribeHandle = this.subscribe('projects', () => initProgressRing());
+});
+
+Template['design'].onRendered(function () {
+    autorunHandle = this.autorun(() => {
+        if (subscribeHandle.ready()) {
             const design = Projects.findOne({ _id: Router.get('id') }).design;
-            if (design) { renderProgressRing(design.progress, design.status); }
-        });
+            renderProgressRing(design.progress, design.status);
+        }
     });
+});
+
+Template['design'].onDestroyed(function () {
+    if (autorunHandle) { autorunHandle.stop(); }
+    if (subscribeHandle) { subscribeHandle.stop(); }
 });
 
 Template['design'].helpers({
     design: () => Projects.findOne({ _id: Router.get('id') }).design,
 });
 
+function initProgressRing() {
+    const arc = d3.arc().outerRadius(radius - 10).innerRadius(radius - 20).startAngle(0).endAngle(2 * Math.PI);
+    const chart = d3.select('#chart');
+    chart.append('path').attr('d', arc).attr('class', 'pg-space');
+    progressRing = chart.append('path').attr('d', arc).attr('class', 'pg-space').attr('id', '#data');
+}
+
 function renderProgressRing(progress: number, status: string) {
-    const radius = 45;
-    const css = [`pg-${status}`, 'pg-space'];
-    const arc = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(radius - 20)
-        .startAngle(0)
-        .endAngle(progress / 100 * 2 * Math.PI);
-    const arc1 = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(radius - 20)
-        .startAngle(0)
-        .endAngle(2 * Math.PI);
-    d3.select('#chart').selectAll('*').remove();
-    d3.select('#chart')
-        .append('path')
-        .attr('d', arc1)
-        .style('fill', 'gray');
-    d3.select('#chart')
-        .append('path')
-        .attr('d', arc)
-        .attr('class', `pg-${status}`);
+    const arc = d3.arc().outerRadius(radius - 10).innerRadius(radius - 20).startAngle(0).endAngle(progress / 100 * 2 * Math.PI);
+    progressRing.attr('d', arc).attr('class', `pg-${status}`);
 }
