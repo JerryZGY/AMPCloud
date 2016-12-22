ac289380edc45b2cbbd5e487f5a7de0008b165c4
Jerry Zang <jerry@ltc.tw>
2016-12-22 18:00:14 +0800

    Modify design page data structure

--- imports/ui/design.ts
+++ imports/ui/design.ts
@@ -1,7 +1,7 @@
 import './design.html';
 import './design.scss';
 import * as d3 from 'd3';
-import { Projects } from '../../lib/collections';
+import { Designs } from '../../lib/collections';
 import { Router } from '../../client/main';
 
 const radius = 45;
@@ -11,14 +11,14 @@ let subscribeHandle: Meteor.SubscriptionHandle = null;
 
 Template['design'].onCreated(function () {
     $('body').attr('class', 'design');
-    subscribeHandle = this.subscribe('projects', () => initProgressRing());
+    subscribeHandle = this.subscribe('designs');
 });
 
 Template['design'].onRendered(function () {
     autorunHandle = this.autorun(() => {
         if (subscribeHandle.ready()) {
-            const design = Projects.findOne({ _id: Router.get('id') }).design;
-            renderProgressRing(design.progress, design.status);
+            const design = Designs.findOne({ projectNo: Router.get('id') }, { sort: { receivedAt: -1 } });
+            setTimeout(() => renderProgressRing(design.progress, design.status), 0);
         }
     });
 });
@@ -29,7 +29,7 @@ Template['design'].onDestroyed(function () {
 });
 
 Template['design'].helpers({
-    design: () => Projects.findOne({ _id: Router.get('id') }).design,
+    design: () => Designs.findOne({ projectNo: Router.get('id') }, { sort: { receivedAt: -1 } }),
 });
 
 function initProgressRing() {
@@ -40,6 +40,7 @@ function initProgressRing() {
 }
 
 function renderProgressRing(progress: number, status: string) {
+    if (!progressRing) { initProgressRing(); }
     const arc = d3.arc().outerRadius(radius - 10).innerRadius(radius - 20).startAngle(0).endAngle(progress / 100 * 2 * Math.PI);
     progressRing.attr('d', arc).attr('class', `pg-${status}`);
 }
