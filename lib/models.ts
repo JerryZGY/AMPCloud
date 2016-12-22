1406a340d4b7a269a430815a7a93f7c2e3362cb2
Jerry Zang <jerry@ltc.tw>
2016-12-22 15:17:09 +0800

    Modify models

--- lib/models.ts
+++ lib/models.ts
@@ -1,76 +1,59 @@
-export type Project = {
-    _id: string;
-    projectNo: string; // 161200011M
+type Status = 'standby' | 'running' | 'done' | 'freeze' | 'error';
+export interface ICollection {
+    _id?: string;
+}
+export interface ILog extends ICollection {
+    projectNo: string;
+    message: string;
+    receivedAt: Date;
+}
+export interface IAMP extends ICollection {
+    projectNo: string;
     projectEvent?: string;
-    logs: string[];
-    design?: Design;
-    designHistory?: Design[];
-    machining?: Machining;
-    machiningHistory?: Machining[];
-    molding?: Molding;
-    moldingHistory?: Molding[];
-    createdAt: Date;
-};
-
-export type Design = {
-    // ======共通資料======
-    projectNo?: string;     // 專案編號 (來自ERP並擷取 - 161200011M)
-    projectEvent?: string;  // 事件內容 (Design start|middle|end)
-    progress?: number;      // 完成進度
-    updatedAt: Date;         // 更新時間
-    // ======取得資料======
-    data?: string;          // 詳細資料
-    status?: 'running' | 'error';        // 流程狀態
-    subStatus?: SubStatus;  // 子流程狀態
-    // ======傳送資料======
+    receivedAt: Date;
+}
+export interface IProject extends IAMP {
     orderNo: string;       // 訂單編號 (來自ERP - 2Y004-16120001)
     templateNo: string;    // 模具樣板 (暫時固定為 - 1)
     customerNo: string;    // 客戶編號 (來自ERP - ABCORP)
     productName: string;   // 產品名稱 (來自ERP - 模具)
     orderDate: Date;       // 訂單時間 (來自ERP - 2016-12-20)
     deadlineDate: Date;    // 完工時間 (來自ERP - 2017-01-16)
-};
-
-export type SubStatus = {
-    MS_Create: StateString;          // 模型建立
-    MS_Surface_Design: StateString;  // 分模面設計
-    MB_Create: StateString;          // 模座建立
-    MB_Detail: StateString;          // 模座細部設計
-    MS_Detail: StateString;          // 機構細部設計
-    MB_Total_Asm: StateString;       // 總組立
-    MB_Asm_Figure: StateString;      // 組立圖
-    MB_Com_Figure: StateString;      // 零件圖
-};
-
-type StateString = 'standby' | 'running' | 'done' | 'freeze' | 'error';
-
-export type Machining = {
-    // ======共通資料======
-    projectNo?: string;
-    projectEvent?: string;
-    progress?: number;
-    updatedAt: Date;
-    // ======取得資料======
-    // ======傳送資料======
-};
-
-export type Molding = {
-    // ======共通資料======
-    projectNo?: string;
-    projectEvent?: string;
-    progress?: number;
-    updatedAt: Date;
-    // ======取得資料======
-    moldTemp?: string;          // 模具溫度
-    startPos?: string;          // 起始位置
-    coolingTime?: string;       // 冷卻時間
-    suckBackStroke?: string;    // 鬆退量值
-    suckBackPressure?: string;  // 背壓量值
-    meltTemp?: string[];        // 熔膠溫度
-    injSpeed?: string[];        // 射出速度
-    injStroke?: string[];       // 計量位置
-    packingPressure?: string[]; // 保壓壓力
-    packingTime?: string[];     // 保壓時間
-    // ======傳送資料======
-    moldNo: string;             // 模具編號
-};
+}
+export interface IStage extends IAMP {
+    status: Status;
+    progress: number;
+}
+export interface IDesign extends IStage {
+    subStatus?: {
+        MS_Create: Status;          // 模型建立
+        MS_Surface_Design: Status;  // 分模面設計
+        MB_Create: Status;          // 模座建立
+        MB_Detail: Status;          // 模座細部設計
+        MS_Detail: Status;          // 機構細部設計
+        MB_Total_Asm: Status;       // 總組立
+        MB_Asm_Figure: Status;      // 組立圖
+        MB_Com_Figure: Status;      // 零件圖
+    };
+}
+export interface IScheduling extends IStage {
+    moldNo: string;
+    production: string;
+    startDate: Date;
+    endDate: Date;
+}
+export interface IMachining extends IStage {
+    machineNo: string;
+}
+export interface IMolding extends IStage {
+    moldTemp?: number;          // 模具溫度
+    startPos?: number;          // 起始位置
+    coolingTime?: number;       // 冷卻時間
+    suckBackStroke?: number;    // 鬆退量值
+    suckBackPressure?: number;  // 背壓量值
+    meltTemp?: number[];        // 熔膠溫度
+    injSpeed?: number[];        // 射出速度
+    injStroke?: number[];       // 計量位置
+    packingPressure?: number[]; // 保壓壓力
+    packingTime?: number[];     // 保壓時間
+}
