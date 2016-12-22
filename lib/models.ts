865d029bcde2cf270d29e92f6b920553f644c6ab
Jerry Zang <jerry@ltc.tw>
2016-12-22 11:32:00 +0800

    Add molding model

--- lib/models.ts
+++ lib/models.ts
@@ -5,27 +5,30 @@ export type Project = {
     logs: string[];
     design?: Design;
     designHistory?: Design[];
-    machinings?: Machining[];
-    moldings?: Molding[];
+    machinings?: Machining;
+    machiningsHistory?: Machining[];
+    moldings?: Molding;
+    moldingsHistory?: Molding[];
     createdAt: Date;
 };
 
 export type Design = {
+    // ======共通資料======
     projectNo?: string;     // 專案編號 (來自ERP並擷取 - 161200011M)
     projectEvent?: string;  // 事件內容 (Design start|middle|end)
-    data?: string;          // 詳細資料
     progress?: number;      // 完成進度
+    updatedAt: Date;         // 更新時間
+    // ======取得資料======
+    data?: string;          // 詳細資料
     status?: 'running' | 'error';        // 流程狀態
     subStatus?: SubStatus;  // 子流程狀態
-    updatedAt: Date;         // 更新時間
-    // ===傳送至子一資料===
-    orderNo?: string;       // 訂單編號 (來自ERP - 2Y004-16120001)
-    templateNo?: string;    // 模具樣板 (暫時固定為 - 1)
-    customerNo?: string;    // 客戶編號 (來自ERP - ABCORP)
-    productName?: string;   // 產品名稱 (來自ERP - 模具)
-    orderDate?: Date;       // 訂單時間 (來自ERP - 2016-12-20)
-    deadlineDate?: Date;    // 完工時間 (來自ERP - 2017-01-16)
-    // ==================
+    // ======傳送資料======
+    orderNo: string;       // 訂單編號 (來自ERP - 2Y004-16120001)
+    templateNo: string;    // 模具樣板 (暫時固定為 - 1)
+    customerNo: string;    // 客戶編號 (來自ERP - ABCORP)
+    productName: string;   // 產品名稱 (來自ERP - 模具)
+    orderDate: Date;       // 訂單時間 (來自ERP - 2016-12-20)
+    deadlineDate: Date;    // 完工時間 (來自ERP - 2017-01-16)
 };
 
 export type SubStatus = {
@@ -39,22 +42,35 @@ export type SubStatus = {
     MB_Com_Figure: StateString;      // 零件圖
 };
 
-type StateString = 'standby' | 'running' | 'done' | 'greeze' | 'error';
+type StateString = 'standby' | 'running' | 'done' | 'freeze' | 'error';
 
 export type Machining = {
+    // ======共通資料======
     projectNo?: string;
-    machiningNo?: string;
-    createdAt?: Date; // 訂單時間
-    endedAt?: Date; // 完工時間
-    sentAt: Date; // 更新時間
-    percent: number;
+    projectEvent?: string;
+    progress?: number;
+    updatedAt: Date;
+    // ======取得資料======
+    // ======傳送資料======
 };
 
 export type Molding = {
+    // ======共通資料======
     projectNo?: string;
-    moldingNo?: string;
-    createdAt?: Date; // 訂單時間
-    endedAt?: Date; // 完工時間
-    sentAt: Date; // 更新時間
-    percent: number;
+    projectEvent?: string;
+    progress?: number;
+    updatedAt: Date;
+    // ======取得資料======
+    moldTemp?: string;          // 模具溫度
+    startPos?: string;          // 起始位置
+    coolingTime?: string;       // 冷卻時間
+    suckBackStroke?: string;    // 鬆退量值
+    suckBackPressure?: string;  // 背壓量值
+    meltTemp?: string[];        // 熔膠溫度
+    injSpeed?: string[];        // 射出速度
+    injStroke?: string[];       // 計量位置
+    packingPressure?: string[]; // 保壓壓力
+    packingTime?: string[];     // 保壓時間
+    // ======傳送資料======
+    moldNo: string;             // 模具編號
 };
