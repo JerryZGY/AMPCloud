f19211da8754ed1e4664fb9f2a49958819d099ef
Jerry Zang <jerry@ltc.tw>
2016-12-22 17:58:58 +0800

    Optimize server methods

--- server/methods.ts
+++ server/methods.ts
@@ -1,48 +1,48 @@
 import * as got from 'got';
-import { Projects } from '../lib/collections';
+import { Projects, Logs, Designs, Schedulings, Machinings, Moldings } from '../lib/collections';
 import * as Models from '../lib/models';
 
 Meteor.methods({
-    'createProject'(data: Models.Project) {
+    'createProject'(data: Models.IProject) {
         const _id = data.projectNo;
-        data.logs = [data.projectEvent];
-        delete data.projectEvent;
-        data.designHistory = [data.design];
-        data.createdAt = new Date();
+        insertLog(data);
         Projects.upsert({ _id }, data);
         Meteor.call('notifyDesign', _id);
     },
     async 'notifyDesign'(_id: string) {
-        const project = Projects.findOne({ _id });
-        Projects.update({ _id }, { $push: { logs: 'Notify design' } });
-        // const opt = {
-        //     body: JSON.stringify({ data: project.design }),
-        //     json: true,
-        //     headers: { 'Content-Type': 'application/json' },
-        // };
-        // const designAPI = 'http://140.135.96.39/icmold/AMPWebService2.asmx/InsertProjInfo';
-        // const response = await got.post(designAPI, opt);
-        // if (response) {
-        //     Projects.update({ _id }, { $push: { logs: 'Notify design' } });
-        // }
+        const data = Projects.findOne({ _id });
+        const url = 'http://140.135.96.39/icmold/AMPWebService2.asmx/InsertProjInfo';
+        // await postToWebService(url, data);
+        Logs.insert({ projectNo: _id, message: 'Notify design', receivedAt: new Date() });
     },
-    'updateDesign'(data: Models.Design) {
-        const _id = data.projectNo;
-        const project = Projects.findOne({ _id });
-        project.logs.push(data.projectEvent);
-        project.designHistory.push(data);
-        data.updatedAt = new Date();
-        project.design = data;
-        Projects.update({ _id }, project);
+    'updateDesign'(data: Models.IDesign) {
+        insertLog(data);
+        Designs.insert(data);
     },
-    'updateMachining'(machining: Models.Machining) {
-        const _id = machining.projectNo;
-        delete machining.projectNo;
-        Projects.update({ _id }, { $push: { machinings: machining } });
+    'updateScheduling'(data: Models.IScheduling) {
+        insertLog(data);
+        // Scheduling.insert(data);
     },
-    'updateMolding'(molding: Models.Molding) {
-        const _id = molding.projectNo;
-        delete molding.projectNo;
-        Projects.update({ _id }, { $push: { moldings: molding } });
+    'updateMachining'(data: Models.IMachining) {
+        insertLog(data);
+        // Machining.insert(data);
+    },
+    'updateMolding'(data: Models.IMolding) {
+        insertLog(data);
+        Moldings.insert(data);
     },
 });
+
+function insertLog(data) {
+    data.receivedAt = new Date();
+    Logs.insert({ projectNo: data.projectNo, message: data.projectEvent, receivedAt: data.receivedAt });
+    delete data.projectEvent;
+}
+
+async function postToWebService(url: string, data) {
+    return await got.post(url, {
+        body: JSON.stringify({ data }),
+        json: true,
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
