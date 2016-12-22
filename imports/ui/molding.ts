5c623baaee7f26755743760c8ddea05c2fa87ab7
Jerry Zang <jerry@ltc.tw>
2016-12-22 18:01:33 +0800

    Implement moldings page

---
+++ imports/ui/molding.ts
@@ -0,0 +1,30 @@
+import './molding.html';
+import './molding.scss';
+import * as c3 from 'c3';
+import { Moldings } from '../../lib/collections';
+import { Router } from '../../client/main';
+
+let subscribeHandle: Meteor.SubscriptionHandle = null;
+
+Template['molding'].onCreated(function () {
+    $('body').attr('class', 'molding');
+    console.log(c3);
+    c3.generate({
+        bindto: '#chart',
+        data: {
+            columns: [
+                ['data1', 30, 200, 100, 400, 150, 250],
+                ['data2', 50, 20, 10, 40, 15, 25],
+            ]
+        },
+    });
+    subscribeHandle = this.subscribe('moldings');
+});
+
+Template['molding'].onDestroyed(function () {
+    if (subscribeHandle) { subscribeHandle.stop(); }
+});
+
+Template['molding'].helpers({
+    moldings: () => Moldings.find({ projectNo: Router.get('id') }, { sort: { receivedAt: -1 }, limit: 100 }),
+});
