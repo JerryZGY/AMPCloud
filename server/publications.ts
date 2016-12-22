7343eb22ae8bb80b7b209726306e57581edbdade
Jerry Zang <jerry@ltc.tw>
2016-12-22 17:58:28 +0800

    Add publications

--- server/publications.ts
+++ server/publications.ts
@@ -1,3 +1,8 @@
-import { Projects } from '../lib/collections';
+import { Logs, Projects, Schedulings, Designs, Machinings, Moldings } from '../lib/collections';
 
-Meteor.publish('projects', () => Projects.find({}));
+Meteor.publish('logs', () => Logs.find());
+Meteor.publish('projects', () => Projects.find());
+Meteor.publish('schedulings', () => Schedulings.find());
+Meteor.publish('designs', () => Designs.find());
+Meteor.publish('machinings', () => Machinings.find());
+Meteor.publish('moldings', () => Moldings.find());
