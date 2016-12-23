752ce1e63dd96ccda9b23dc880d8c059169885db
Jerry Zang <jerry@ltc.tw>
2016-12-23 12:40:33 +0800

    Optimize project template helpers

--- imports/ui/project.ts
+++ imports/ui/project.ts
@@ -1,14 +1,40 @@
 import './project.html';
 import './project.scss';
+import { Mongo } from 'meteor/mongo';
 import { Router } from '../../client/main';
-import { Projects } from '../../lib/collections';
+import * as Models from '../../lib/models';
+import { Designs, Schedulings, Machinings, Moldings } from '../../lib/collections';
+
+let subscribeHandles: Meteor.SubscriptionHandle[] = [];
+Template['project'].onCreated(function () {
+    subscribeHandles = [
+        this.subscribe('designs'),
+        this.subscribe('schedulings'),
+        this.subscribe('machinings'),
+        this.subscribe('moldings'),
+    ];
+});
+
+Template['project'].onDestroyed(function () {
+    subscribeHandles.forEach(handle => handle.stop());
+});
 
 Template['project'].helpers({
-    pgDesignStatus: () => parseStatus('designStatus'),
-    pgSchedulingStatus: () => parseStatus('schedulingStatus'),
-    pgMachiningStatus: () => parseStatus('machiningStatus'),
-    pgMoldingStatus: () => parseStatus('moldingStatus'),
+    design: () => getDataAndRenderProgressBar('design', Designs),
+    scheduling: () => getDataAndRenderProgressBar('scheduling', Schedulings),
+    machining: () => getDataAndRenderProgressBar('machining', Machinings),
+    molding: () => getDataAndRenderProgressBar('molding', Moldings),
+    parseStatus: status => status === 'running' || status === 'error' ? status : 'space',
 });
+
+function getDataAndRenderProgressBar(name: string, collection: Mongo.Collection<Models.IStage>) {
+    const options = { sort: { receivedAt: -1 } };
+    const defaultData = { status: 'standby', progress: 0 };
+    const data = collection.findOne({ projectNo: Template.currentData()._id }, options) || defaultData;
+    const templateInstance: any = Template.instance();
+    if (templateInstance.view.isRendered) { templateInstance.$(`.${name} .progress`).data('progress').set(data.progress); }
+    return data;
+}
 
 Template['project'].events({
     'click .ctrl'(e, tmpl) {
@@ -27,12 +53,3 @@ Template['project'].events({
         Router.go(`/molding/${tmpl.data._id}`);
     },
 });
-
-function parseStatus(section: string) {
-    const status = Template.currentData()[section];
-    if (status) {
-        return status === 'running' || status === 'error' ? status : 'space';
-    } else {
-        return 'space';
-    }
-}
