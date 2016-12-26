847ff4c0dc51282b4c00958a9b9e94e42b87e0cf
Jerry Zang <jerry@ltc.tw>
2016-12-26 18:50:44 +0800

    Add sendModels

---
+++ lib/sendModels.ts
@@ -0,0 +1,13 @@
+export interface IDesign {
+    projectNo: string;
+    moldNo: string;
+    templateNo: string;
+    customerNo: string;
+    productName: string;
+    orderDate: Date;
+    deadlineDate: Date;
+    receivedAt: Date;
+}
+export interface IMolding {
+    projectNo: string;
+}
