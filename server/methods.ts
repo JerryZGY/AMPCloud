48d41b962712a5238ecad374f8d29c6aa659bb00
Jerry Zang <jerry@ltc.tw>
2016-12-26 18:51:26 +0800

    Implement scheduling and machining update meteor method

--- server/methods.ts
+++ server/methods.ts
@@ -1,5 +1,5 @@
 import * as got from 'got';
-import { Projects, Logs, Designs, Schedulings, Machinings, Moldings } from '../lib/collections';
+import { Projects, Logs, Designs, Schedulings, Moldings } from '../lib/collections';
 import * as Models from '../lib/models';
 
 Meteor.methods({
@@ -10,22 +10,44 @@ Meteor.methods({
         Meteor.call('notifyDesign', _id);
     },
     async 'notifyDesign'(_id: string) {
+        console.log('notify');
         const data = Projects.findOne({ _id });
         const url = 'http://140.135.96.39/icmold/AMPWebService2.asmx/InsertProjInfo';
-        // await postToWebService(url, data);
-        Logs.insert({ projectNo: _id, message: 'Notify design', receivedAt: new Date() });
+        // const url = 'http://localhost:8888';
+        delete data._id;
+        delete data.spec;
+        let response;
+        try {
+            response = await postToWebService(url, data);
+            console.log('response', response);
+        } catch (error) {
+            response = error;
+            console.log('error', response);
+        }
+        Logs.insert({ projectNo: _id, message: `Notify design: ${response}`, receivedAt: new Date() });
     },
     'updateDesign'(data: Models.IDesign) {
         insertLog(data);
         Designs.insert(data);
     },
-    'updateScheduling'(data: Models.IScheduling) {
-        insertLog(data);
-        // Scheduling.insert(data);
+    'updateScheduling'(data: Models.IScheduling[]) {
+        data.forEach(datum => {
+            const selector = { projectNo: datum.projectNo, partNo: datum.partNo };
+            const scheduling = Schedulings.findOne(selector) || datum;
+            insertLog(scheduling);
+            Schedulings.upsert(selector, scheduling);
+        });
     },
-    'updateMachining'(data: Models.IMachining) {
+    'updateMachining'(data: Models.IScheduling) {
+        const selector = { projectNo: data.projectNo, partNo: data.partNo };
+        const datum = Schedulings.findOne(selector);
+        const startTime = data.startTime;
+        if (startTime) { datum.startTime = startTime; }
+        const endTime = data.endTime;
+        if (endTime) { datum.endTime = endTime; }
+        if (datum.error.length && data.error) { datum.error.push(data.error); }
         insertLog(data);
-        // Machining.insert(data);
+        Schedulings.update(selector, datum);
     },
     'updateMolding'(data: Models.IMolding) {
         insertLog(data);
