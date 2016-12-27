afe32a9f9070fd7e62fa1c501cfc8bc77121cfd8
Jerry Zang <jerry@ltc.tw>
2016-12-27 18:08:43 +0800

    Modify ctrl view

--- client/view/ctrl.ts
+++ client/view/ctrl.ts
@@ -1,6 +1,17 @@
 import './ctrl.html';
 import './ctrl.scss';
+import { Logs } from '../../lib/collections';
 
-Template['ctrl'].onCreated(() => {
+let subscribeHandle: Meteor.SubscriptionHandle = null;
+Template['ctrl'].onCreated(function () {
     $('body').attr('class', 'ctrl');
+    subscribeHandle = this.subscribe('logs');
+});
+
+Template['ctrl'].onDestroyed(function () {
+    if (subscribeHandle) { subscribeHandle.stop(); }
+});
+
+Template['ctrl'].helpers({
+    logs: () => Logs.find({}, { sort: { receivedAt: -1 } }),
 });
