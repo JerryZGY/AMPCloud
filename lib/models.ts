dd53d4119e91a1f3e2b08364a3f733d8e150d7da
Jerry Zang <jerry@ltc.tw>
2016-12-27 10:40:02 +0800

    Add error type

--- lib/models.ts
+++ lib/models.ts
@@ -1,25 +1,36 @@
 type Status = 'standby' | 'running' | 'done' | 'freeze' | 'error';
+type Error = {
+    workOrder: string;          // 專案編號
+    machineNo: string;          // 機台編號
+    errorType: string;          // 異常狀況
+    SpindleSpeed: string;       // 轉速
+    Feed: string;               // 進給
+    Cutters: string;            // 刀刃數
+    Depth: string;              // 切深
+    Threshold: string;          // 門檻值
+    NC_code: string;            // NC碼
+}
 export interface ICollection {
-    _id?: string;
+    _id?: string;               // DB識別碼
 }
 export interface ILog extends ICollection {
-    projectNo: string;
-    message: string;
-    receivedAt: Date;
+    projectNo: string;          // 專案編號
+    message: string;            // 記錄訊息
+    receivedAt: Date;           // 接收時間
 }
 export interface IAMP extends ICollection {
-    projectNo: string;
-    projectEvent?: string;
-    receivedAt: Date;
+    projectNo: string;          // 專案編號
+    projectEvent?: string;      // 專案事件
+    receivedAt: Date;           // 接收時間
 }
 export interface IProject extends IAMP {
     moldNo: string;             // 模具編號
     spec: string;               // 規格描述
-    templateNo: string;         // 模具樣板 (暫時固定為 - 1)
-    customerNo: string;         // 客戶編號 (來自ERP - ABCORP)
-    productName: string;        // 產品名稱 (來自ERP - 模具)
-    orderDate: Date;            // 訂單時間 (來自ERP - 2016-12-20)
-    deadlineDate: Date;         // 完工時間 (來自ERP - 2017-01-16)
+    templateNo: string;         // 模具樣板
+    customerNo: string;         // 客戶編號
+    productName: string;        // 產品名稱
+    orderDate: Date;            // 訂單日期
+    deadlineDate: Date;         // 完工日期
 }
 export interface IStage extends IAMP {
     status: Status;             // 處理狀況
@@ -35,7 +46,7 @@ export interface IDesign extends IStage {
     MB_Asm_Figure?: Status;     // 組立圖
     MB_Com_Figure?: Status;     // 零件圖
 }
-export interface IScheduling extends IStage {
+export interface IComponent extends IStage {
     partNo?: string;            // 零件編號
     machineNo?: string;         // 機台編號
     method?: string;            // 製程名稱
@@ -43,7 +54,7 @@ export interface IScheduling extends IStage {
     expectedEndTime?: Date;     // 預估結束時間
     startTime?: Date;           // 開始時間
     endTime?: Date;             // 結束時間
-    error?: any[];              // 錯誤資訊
+    error?: Error[];            // 錯誤訊息
 }
 export interface IMolding extends IStage {
     moldTemp?: number;          // 模具溫度
