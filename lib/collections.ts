2cb8ef53f989f11f34090ee2ac7caa505f4c3bb2
Jerry Zang <jerry@ltc.tw>
2016-12-28 16:31:07 +0800

    Implement design view moldNo

--- lib/collections.ts
+++ lib/collections.ts
@@ -3,6 +3,11 @@ import * as Models from './models';
 
 export const Logs = new Mongo.Collection<Models.ILog>('logs');
 export const Projects = new Mongo.Collection<Models.IProject>('projects');
-export const Designs = new Mongo.Collection<Models.IDesign>('designs');
+export const Designs = new Mongo.Collection<Models.IDesign>('designs', {
+    transform(design) {
+        design.moldNo = Projects.findOne({_id: design.projectNo}).moldNo.split(',');
+        return design;
+    },
+});
 export const Parts = new Mongo.Collection<Models.IPart>('parts');
 export const Moldings = new Mongo.Collection<Models.IMolding>('moldings');
