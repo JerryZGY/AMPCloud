ab3c875a08b640f228f89eb2d1fa6a28fb7983f2
Jerry Zang <jerry@ltc.tw>
2016-12-28 18:36:00 +0800

    Implement molding view chart

--- client/view/molding.ts
+++ client/view/molding.ts
@@ -5,8 +5,7 @@ import * as Models from '../../lib/models';
 import { Router } from '../main';
 import { Moldings, Designs } from '../../lib/collections';
 
-let chart = null;
-const options = { sort: { receivedAt: -1 }, limit: 100 };
+let charts = [];
 let autorunHandle: Tracker.Computation = null;
 let subscribeHandle: Meteor.SubscriptionHandle = null;
 
@@ -18,54 +17,86 @@ Template['molding'].onCreated(function () {
 Template['molding'].onRendered(function () {
     autorunHandle = this.autorun(() => {
         if (subscribeHandle.ready()) {
-            const moldings = Moldings.find({ projectNo: Router.get('id') }, options).fetch();
+            const moldings = formatMoldings(Moldings.find({ projectNo: Router.get('id') }, { sort: { receivedAt: 1 }, limit: 100 }).fetch());
             setTimeout(() => renderChart(moldings), 0);
         }
     });
 });
 
 Template['molding'].onDestroyed(function () {
-    chart = null;
+    charts = [];
     if (autorunHandle) { autorunHandle.stop(); }
     if (subscribeHandle) { subscribeHandle.stop(); }
 });
 
 Template['molding'].helpers({
-    moldings: () => Moldings.find({ projectNo: Router.get('id') }, options),
+    moldings: () => Moldings.find({ projectNo: Router.get('id') }, { sort: { receivedAt: -1 }, limit: 50 }),
+    parseArray: (data: string[]) => data.map(datum => datum ? datum : '0').join(', '),
 });
 
-function renderChart(moldings: Models.IMolding[]) {
-    let meltTemp1 = [];
-    let meltTemp2 = [];
-    let meltTemp3 = [];
-    let meltTemp4 = [];
-    let meltTemp5 = [];
-    moldings.forEach((molding: Models.IMolding) => {
-        meltTemp1.push(molding.meltTemp[0]);
-        meltTemp2.push(molding.meltTemp[1]);
+type Molding = {
+    meltTemp: number[][];
+    injSpeed: number[][];
+    packingPressure: number[][];
+};
+
+enum Level { '一', '二', '三', '四', '五' }
+
+/**
+ * Input: [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
+ * Output: [[1, 3, 5, 7, 9], [2, 4, 6, 8, 10]]
+ */
+function transformDimension(source: number[][]) {
+    let results = [];
+    source.forEach(src => {
+        src.forEach((x, columnsIndex) => {
+            results[columnsIndex] = source.map((y, dataIndex) => source[dataIndex][columnsIndex] || 0);
+        });
     });
-    if (!chart) {
-        chart = c3.generate({
-            bindto: '#chart',
-            data: {
-                columns: [
-                    ['一段熔溫', ...meltTemp1],
-                    ['二段熔溫', ...meltTemp2],
-                    ['三段熔溫', ...meltTemp3],
-                    ['四段熔溫', ...meltTemp4],
-                    ['五段熔溫', ...meltTemp5],
-                ]
-            },
+    return results;
+}
+
+function formatMoldings(moldings: Models.IMolding[]): Molding {
+    const data = {
+        meltTemp: [],           // 熔膠溫度
+        injSpeed: [],           // 射出速度
+        packingPressure: [],    // 保壓壓力
+    };
+    moldings.forEach(molding => {
+        data.meltTemp.push(molding.meltTemp);
+        data.injSpeed.push(molding.injSpeed);
+        data.packingPressure.push(molding.packingPressure);
+    });
+    data.meltTemp = transformDimension(data.meltTemp);
+    data.injSpeed = transformDimension(data.injSpeed);
+    data.packingPressure = transformDimension(data.packingPressure);
+    return data;
+}
+
+function renderChart(molding: Molding) {
+    const meltTempColumns = molding.meltTemp.map((array, index) => [`熔膠溫度${Level[index]}`, ...array]);
+    const injSpeedColumns = molding.injSpeed.map((array, index) => [`射出速度${Level[index]}`, ...array]);
+    const packingPressureColumns = molding.packingPressure.map((array, index) => [`保壓壓力${Level[index]}`, ...array]);
+    if (!charts.filter(Boolean).length) {
+        const meltTempChart = c3.generate({
+            bindto: '#meltTemp',
+            data: { columns: meltTempColumns },
+            axis: { x: { label: { text: '模次', position: 'outer-center' } }, y: { label: { text: '溫度', position: 'outer-middle' } } },
         });
+        charts.push(meltTempChart);
+        const injSpeedChart = c3.generate({
+            bindto: '#injSpeed',
+            data: { columns: injSpeedColumns },
+        });
+        charts.push(injSpeedChart);
+        const packingPressureChart = c3.generate({
+            bindto: '#packingPressure',
+            data: { columns: packingPressureColumns },
+        });
+        charts.push(packingPressureChart);
     } else {
-        chart.load({
-            columns: [
-                ['一段熔溫', ...meltTemp1],
-                ['二段熔溫', ...meltTemp2],
-                ['三段熔溫', ...meltTemp3],
-                ['四段熔溫', ...meltTemp4],
-                ['五段熔溫', ...meltTemp5],
-            ]
-        });
+        charts[0].load({ columns: meltTempColumns });
+        charts[1].load({ columns: injSpeedColumns });
+        charts[2].load({ columns: packingPressureColumns });
     }
 }
