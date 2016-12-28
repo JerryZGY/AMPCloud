3a46edd6c0c46952e282370917a0db8c27284bf7
Jerry Zang <jerry@ltc.tw>
2016-12-28 16:40:32 +0800

    Implement machining view error message

--- client/view/machining.ts
+++ client/view/machining.ts
@@ -16,6 +16,28 @@ Template['machining'].onDestroyed(function () {
 
 Template['machining'].helpers({
     parts: () => Parts.find({ projectNo: Router.get('id') }, { sort: { receivedAt: -1 }, limit: 100 }),
+    sample: () => [
+        {
+            type: 'Chatter',
+            spindleSpeed: '5000',
+            feed: '1500',
+            cutters: '4',
+            depth: '0.2',
+            threshold: '-',
+            ncCode: 'N65 X77.789 Y50.156 Z-.2',
+            startTime: '2016-12-28 16:13:33Z',
+        },
+        {
+            type: 'Chatter',
+            spindleSpeed: '5000',
+            feed: '1900',
+            cutters: '2',
+            depth: '0.1',
+            threshold: '-',
+            ncCode: 'N35 X77.789 Y20.136 Z-.1',
+            endTime: '2016-12-28 16:15:33Z',
+        },
+    ],
 });
 
 Template['machining'].events({
