ec6620391b58a9af3a51365b38a658ebaf680e04
Jerry Zang <jerry@ltc.tw>
2016-12-29 12:04:01 +0800

    Optimize machining view

--- client/view/machining.ts
+++ client/view/machining.ts
@@ -16,38 +16,15 @@ Template['machining'].onDestroyed(function () {
 
 Template['machining'].helpers({
     parts: () => Parts.find({ projectNo: Router.get('id') }, { sort: { receivedAt: -1 }, limit: 100 }),
-    sample: () => [
-        {
-            type: 'Chatter',
-            spindleSpeed: '5000',
-            feed: '1500',
-            cutters: '4',
-            depth: '0.2',
-            threshold: '-',
-            ncCode: 'N65 X77.789 Y50.156 Z-.2',
-            startTime: '2016-12-28 16:13:33Z',
-        },
-        {
-            type: 'Chatter',
-            spindleSpeed: '5000',
-            feed: '1900',
-            cutters: '2',
-            depth: '0.1',
-            threshold: '-',
-            ncCode: 'N35 X77.789 Y20.136 Z-.1',
-            endTime: '2016-12-28 16:15:33Z',
-        },
-    ],
+    hasError: (err) => err && err.length ? 'error' : '',
+    splice: (arr = []) => arr.splice(0, 5),
 });
 
 Template['machining'].events({
-    'click .info'(e, tmpl) {
-        // console.log('e', this);
-        // console.log('target', e.currentTarget);
-        showDialog();
-    },
+    'click .info'() { showDialog(this._id); },
 });
 
-function showDialog() {
-    $('#dialog').data('dialog').open();
+function showDialog(id) {
+    const target = $(`#${id}>div`);
+    if (target.length) { target.data('dialog').open(); }
 }
