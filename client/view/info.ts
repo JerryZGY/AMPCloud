41421e4e124fec0f6772ccd44c4537afca54948f
Jerry Zang <jerry@ltc.tw>
2016-12-30 14:35:05 +0800

    Implement info view

--- client/view/info.ts
+++ client/view/info.ts
@@ -1,11 +1,17 @@
 import './info.html';
 import './info.scss';
+import * as ampcloud from '../../package.json';
 
 Template['info'].onCreated(function () {
     $('body').attr('class', 'info');
 });
 
 Template['info'].helpers({
+    info: () => {
+        return {
+            version: ampcloud.version,
+            designUrl: Meteor.settings.public['designUrl'],
+        };
+    },
     // logs: () => Logs.find({}, { sort: { receivedAt: -1 } }),
 });
-
