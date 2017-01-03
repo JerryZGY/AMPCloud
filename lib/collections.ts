b17f354db4a9e0b0da8294c195c31d6273f14d7d
Jerry Zang <jerry@ltc.tw>
2017-01-03 10:21:37 +0800

    Add moldings collection before insert func

--- lib/collections.ts
+++ lib/collections.ts
@@ -1,13 +1,13 @@
-import { Mongo } from 'meteor/mongo';
 import * as Models from './models';
 
 export const Logs = new Mongo.Collection<Models.ILog>('logs');
 export const Projects = new Mongo.Collection<Models.IProject>('projects');
 export const Designs = new Mongo.Collection<Models.IDesign>('designs', {
     transform(design) {
-        design.moldNo = Projects.findOne({_id: design.projectNo}).moldNo.split(',');
+        design.moldNo = Projects.findOne({ _id: design.projectNo }).moldNo.split(',');
         return design;
     },
 });
 export const Parts = new Mongo.Collection<Models.IPart>('parts');
 export const Moldings = new Mongo.Collection<Models.IMolding>('moldings');
+Moldings.before.insert(function (userId, doc) { doc.timeIndex = new Date(doc.timeIndex); });
